9f1a70f9dc70d14def03df5b68df6f1827f573dd
Volya23 <v.o.kryzhanivska@gmail.com>
2023-06-04 01:12:49 +0300

    task2

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-let box1;
-box1 = null;
-console.log(box1);
+let r = 15;
 
-let box2 = 5;
-console.log(box2);
-
-let box3 = '2'
-console.log(box3);
-
-let box4 = box2 + box3;
-console.log(box4);
-console.log(typeof box4);
-
-let box5 = box3 - box2;
-console.log(box5);
-console.log(typeof box5);
-
-let box6 = box1 + box2;
-console.log(box6);
-console.log(typeof box6);
-
-let box7 = 10 +undefined;
-console.log(box7);
-console.log(typeof box7);
-
-const motto = 'У цілого війська девіз був один: "За волю, за рідну країну!" (Леся Українка)'
-console.log(motto);
-console.log(typeof motto);
+if(r > 12) {
+    console.log('більше 12');
+} else if (r < 5) {
+    console.log('менша за 5');
+} else {
+    console.log('в діапазоні між 5 і 12');
+}
