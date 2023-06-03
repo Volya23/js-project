bf2888d824b9caeddf9362d57dda914f7a3d7262
Volya23 <v.o.kryzhanivska@gmail.com>
2023-06-03 22:38:32 +0300

    task1

---
+++ index.js
@@ -0,0 +1,27 @@
+let box1;
+box1 = null;
+console.log(box1);
+
+let box2 = 5;
+console.log(box2);
+
+let box3 = '2'
+console.log(box3);
+
+let box4 = box2 + box3;
+console.log(box4);
+console.log(typeof box4);
+
+let box5 = box3 - box2;
+console.log(box5);
+console.log(typeof box5);
+
+let box6 = box1 + box2;
+console.log(box6);
+console.log(typeof box6);
+
+let box7 = 10 +undefined;
+console.log(box7);
+
+const motto = 'У цілого війська девіз був один: "За волю, за рідну країну!" (Леся Українка)'
+console.log(motto);
