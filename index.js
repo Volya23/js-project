ca01879f7ba05c8e12ba67e32b779c0257d4ca7b
Volya23 <v.o.kryzhanivska@gmail.com>
2023-06-04 17:09:13 +0300

    Logic task

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
-let r = 15;
+// задача 1
 
-if(r > 12) {
-    console.log('більше 12');
-} else if (r < 5) {
-    console.log('менша за 5');
-} else {
-    console.log('в діапазоні між 5 і 12');
+let min = 20;
+
+if (min <= 15) {
+console.log('перша чверть');
+} else if (min > 15 && min <= 30) {
+    console.log('друга чверть');
+} else if (min > 30 && min <= 45) {
+    console.log('третя чверть');
+} else if (min > 45 && min <= 60) {
+    console.log('четверта чверть');
 }
+
+
+// задача 2
+
+let num1 = 70;
+let num2 = 25;
+
+if (num2 > num1) {
+    console.log(num2 > num1);
+} else {
+    console.log(num1 > num2);
+} 
+
+// задача 3
+
+let year = 2023;
+
+if (year % 4 && year % 400) {
+    console.log('високосний рік');
+} else if (year % 400) {
+    console.log('високосний рік');
+} else {
+    console.log('невисокосний рік');
+}
+
+// задача 4
+
+let mini = 120;
+let max = 2345;
+let num = 56;
+
+if (num <= 2345 && num >= 120) {
+    console.log('В діапазоні');
+} else {
+    console.log('Поза діапазоном');
+}
+
+// задача 5
+
+let pen = m;
+let note = k;
