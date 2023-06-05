4efd9fa72c5f3d942659690d6ef3f611891c24dc
Volya23 <v.o.kryzhanivska@gmail.com>
2023-06-05 23:02:00 +0300

    function tasks

--- index.js
+++ index.js
@@ -5,3 +5,41 @@ function math (num) {
 math (4);
 math (35);
 math (4567);
+
+
+// задача 1
+
+let num1 = Number(prompt('Введіть перше число'));
+let num2 = Number(prompt('Введіть друге число'));
+
+function moreNumber (arg1, arg2) {
+    const result = arg1 > arg2 || arg2 > arg1;
+    return result;
+}
+let result = moreNumber (num1, num2);
+console.log(result);
+
+
+// задача 2
+
+/*let num1 = Number(prompt('Введіть перше число'));
+let num2 = Number(prompt('Введіть друге число'));
+
+function middleMeaning (arg1, arg2) {
+    const result = (arg1 + arg2)/2;
+    return result;
+}
+let result = middleMeaning (num1, num2);
+console.log(result);*/
+
+// задача 3
+
+/*let numb1 = Number(prompt('Введіть перше число'));
+let numb2 = Number(prompt('Введіть друге число'));
+
+function PairNumber (arg1, arg2) {
+    const result = arg1 % 2 || arg2 % 2;
+    return result;
+}
+let result = PairNumber (numb1, numb2);
+console.log(result);*/
