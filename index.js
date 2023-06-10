d6f47f40e8e64fbd2115438c676e9e60dfba8658
Volya23 <v.o.kryzhanivska@gmail.com>
2023-06-10 22:52:30 +0300

    password

--- index.js
+++ index.js
@@ -1,141 +1,18 @@
-const isAuth = (false);
-const viewMode = isAuth ? 'autAuth' : 'unAuth';
-console.log(viewMode);
+let password = 'qwerty123';
 
-// вправа switch.case
+//варіант 1
 
-const menu = prompt('Оберіть варіант: \n 1 - Сік, \n 2 - Вода, \n 3 - Кава, \n 4 - Чай, \n 5 - Лимонад');
+/*let userpassword = prompt ('Введіть свій пароль')
+while (userpassword != password) {
+    userpassword = prompt ('Невірний пароль! Введіть ще раз!')
+}*/
 
-switch (menu) {
-    case '1': {
-        console.log('Сік');
+ // варіант 2
+
+ while (true) {
+    let userpass = prompt ('Введіть свій пароль');
+    if (userpass === password) {
         break;
     }
-    case '2': {
-        console.log('Вода');
-        break;
-    }
-    case '3': {
-        console.log('Кава');
-        break;
-    }
-    case '4': {
-        console.log('Чай');
-        break;
-    }
-    case '5': {
-        console.log('Лимонад');
-        break;
-    }
-    default: {
-        console.log('Введіть число від 1 до 5');
-    }
-        break;
-}
-
-// вправа HOF
-
-function washingFace() {
-    // ............
-    console.log('Washing the face');
-  }
-  
-  function brushingTeeth() {
-    // ............
-    console.log('Brushing teeth');
-  }
-  
-  function skincare() { // догляд за шкірою
-    // ............
-    console.log('Skincare');
-  }
-  
-  function cooking() {
-    // ............
-    console.log('Cooking');
-  }
-  
-  function sleep() {
-    // ............
-    console.log('Sleep');
-  }
-  
-  function breakfast() {
-    // ............
-    console.log('Breakfast');
-  }
-  
-  function education() {
-    // ............
-    console.log('Education');
-  }
-  
-  function lunch() { // Обід
-    // ............
-    console.log('Lunch');
-  }
-  
-  function dinner() {
-    // ............
-    console.log('Dinner');
-  }
-  
-  function walk() { // Прогулянка
-    // ............
-    console.log('Walk');
-  }
-  
-  function shower() {
-    // ............
-    console.log('Shower');
-  }
-  
-  function todo(timeOfDay) { // Higher-order function
-    switch(timeOfDay) {
-
-      case 'morning': {
-
-        console.log('>>>...MONING...<<<');
-  
-        washingFace();
-        brushingTeeth();
-        skincare();
-        breakfast();
-  
-        break;
-      }
-      case 'lunch': {
-        
-        console.log('>>>...LUNCH...<<<');
-  
-        education();
-        lunch();
-        cooking();
-        walk();
-  
-        break;
-      }
-      case 'evening': {
-
-        console.log('>>>...EVENING...<<<');
-  
-        education();
-        dinner();
-        walk();
-        shower();
-        brushingTeeth();
-        washingFace();
-        skincare();
-        sleep();
-  
-        break;
-      }
-      default: {
-        console.log('Error! Enter a valid time of day!');
-      } 
-    }
-  }
-  
-  todo('morning');
-  todo('lunch');
-  //todo('evening');
+ }
+ 
