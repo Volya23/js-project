3e0f27bf47d9e293f6bca4864d4aea4670e61891
Volya23 <v.o.kryzhanivska@gmail.com>
2023-06-14 16:08:59 +0300

    change tasks for...in

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-/*const teacher = {
-    name: 'МарьІванна',
-    nickname: 'Snowman',
-    subject: 'математика',
-    teach: function () {
-        return 'I am teaching';
-    }
-
-}*/
- 
-/*function Country(name, population, capital, area) {
-    this.name = name;
-    this.population = population;
-    this.capital = capital;
-    this.area = area;
-
-    this.getDensity = function() {
-        return this.population / this.area;
-    }
-}
-const Ukraine = new Country('Ukraine', 36744636, 'Kyiv', 603628);
-console.log(Ukraine.getDensity()); */
-
-
-/*function greetingUser(user) {
-    return `Привіт, ${user.name} ${user.surname}. В наступному році тобі буде ${user.age + 1}`
-}
-
-const user = {
-    name: 'John',
-    surname: 'Doe',
-    age: 35
-}*/
-
-
 // task 1
 
 /*const user = {
@@ -56,15 +21,16 @@ const departmentSalary = {
     HR: 20000,
     development: 100000000,
     drivers: 3000,
-    assistans: 6000,
+    assistans: 10000,
     marketing: undefined
 }
 
 function total(departmentSalary) {
     let sum = 0;
+
     for(let key in departmentSalary) {
         if (typeof departmentSalary [key] === 'number') {
-            sum += departmentSalary [key];
+            sum = sum + departmentSalary [key];
         }
     }
     return sum;
