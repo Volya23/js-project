14cc10a9661e1c795fd39a655277c88c33d9a3ae
Volya23 <v.o.kryzhanivska@gmail.com>
2023-06-24 23:47:53 +0300

    task MyStairs

--- index.js
+++ index.js
@@ -1,56 +1,37 @@
-//(sort)
+/*Задача: створити функцію-конструктор для сходів.
 
-//Створіть масив чисел. Відсортуйте його у порядку зростання / спадання
+Об'єкт має властивість:
+currentStair // сходинка, на якій ми зараз знаходимось. Початково = 0
 
-const array = [5, 9, 7, 45, 16];
-    array.sort (function (a, b) {
-        return a - b;
-    });
-    console.log(array);
+Має методи (прототип):
+up() - піднімає на сходинку вище
+down() - опускає на сходинку нижче
+showStair() - показує, на якій сходинці ми зараз знаходимось */
 
-    array.sort (function (a, b) {
-        return b - a;
-    });
-    console.log(array);
+function MyStairs() {
+    this.currentStair = 0;
+}
+MyStairs.prototype = new MyStairProto();
 
-
-//(filter)
-
-//Створити новий масив, в який увійдуть тільки парні елементи початкового масиву
-
-const array1 = [3, 8, 67, 23, 6];
-    function pairNum (num) {
-        if (num % 2 === 0) {
-            return true
-        } else {
-            return false
+function MyStairProto() {
+    
+    this.up = function () {
+            this.currentStair++;
+        }
+    
+    this.down = function () {
+        if (this.currentStair > 0) {
+            this.currentStair--;
         }
     }
-console.log(array1.filter(pairNum));
-
-
-//(reduce)
-
-//У вас є масив чисел. * Знайдіть суму елементів масиву. ** Знайдіть суму тільки парних елементів масиву
-
-// *
-const array2 = [5, 90, 76, 4, 9, 4];
-    function red(a, b) {
-        return a + b
-    };
-    
-const sum = array2.reduce(red, 0);
-console.log(sum);
    
-// **
+    this.showStair = function () {
+        return this.currentStair;
+        }
+    }
 
-const array21 = [5, 90, 76, 4, 9, 4];
-        const tr1 = array21.reduce ((sum, key) => {
-            if (key %2 === 0) {
-                sum += key;
-            }
-            return sum;
-        }, 0);
-        console.log(tr1);
+    const ld = new MyStairs ();
+
+
 
 
