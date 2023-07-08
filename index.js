496b74f733493706fa12f07b2831091e5b6671d4
Volya23 <v.o.kryzhanivska@gmail.com>
2023-07-08 18:37:11 +0300

    rest/spread

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
-/*Задача: створити функцію-конструктор для сходів.
+/* (rest)
 
-Об'єкт має властивість:
-currentStair // сходинка, на якій ми зараз знаходимось. Початково = 0
-
-Має методи (прототип):
-up() - піднімає на сходинку вище
-down() - опускає на сходинку нижче
-showStair() - показує, на якій сходинці ми зараз знаходимось */
-
-function MyStairs() {
-    this.currentStair = 0;
-}
-MyStairs.prototype = new MyStairProto();
-
-function MyStairProto() {
-    
-    this.up = function () {
-            this.currentStair++;
-        }
-    
-    this.down = function () {
-        if (this.currentStair > 0) {
-            this.currentStair--;
-        }
-    }
-   
-    this.showStair = function () {
-        return this.currentStair;
-        }
-    }
-
-    const ld = new MyStairs ();
+Напишіть функцію, яка приймає масив і сумує всі числа в цьому масиві (reduce).
+Функція має бути стрілкова */
 
 
+const arrowFun = (...rest) => rest.reduce ((total, amount) => total + amount);
 
 
+/*(spread)
+
+Є 2 масиви:
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [10, 11, 12, 13, 14, 15];
+
+Задача:
+Зклейте два масиви в один, не використовуючи concat */
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [10, 11, 12, 13, 14, 15];
+const arr = [... arr1, ... arr2];
